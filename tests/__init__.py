2326e6b6a822129c580e63f190e77a76e708c8c1
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2017-09-15 19:16:05 +0200

    Add a script to condionally load/run asyncio tests (Python3 only).
    Using skip decorators on the test classes doesn't work, as
    the files are parsed prior to execution, and we end errors
    due to the 'async' word not being recognized under Python 2.

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+TEST_NAMES = [
+    'test_activate_deactivate',
+    'test_active_span_replacement',
+    'test_client_server',
+    'test_common_request_handler',
+    'test_late_span_finish',
+    'test_multiple_callbacks',
+    'test_nested_callbacks',
+]
